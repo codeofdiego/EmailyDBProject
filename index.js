965acaacb88c32fd2eda724139c4cb321a18b2d8
Diego Oliveira <dhdantas@gmail.com>
2018-04-03 18:46:59 -0300

    Changed

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'})
-})  
+  res.send({hi: 'sup'})
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
